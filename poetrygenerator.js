5b0da78818b65ce6c04b235493fd01e7c618ecd6
VaporShark <tharlow29@gmail.com>
2019-02-15 14:13:16 -0600

    Add files via upload

---
+++ poetrygenerator.js
@@ -0,0 +1,14 @@
+{
+	"origin": ["#subject# #adjectives# #nouns# #verbs# the #plrnouns#    \n  #opener# #subject# #nouns# #verbs# and #verbs# \n#subject# #adjectives# #nouns# #verbs# \n \n  #questionOpener# #question# #adjectives# #plrnouns# #presverbs# \n #opener# #adjectives# #plrnouns# #presverbs# \n #questionOpener# #question# #adjectives# #plrnouns# #presverbs# \n #opener# #adjectives# #plrnouns# #presverbs#"]
+, 	"subject": ["a", "the"]
+, 	"questionOpener": ["where", "how"]
+,	"question":  ["do"]
+,	"connector": ["and", "with", "where"]
+,	"opener":["when", "although", "while", "as"]
+,	"verbs": ["bled", "faded", "writhed", "flowed", "commiserated", "ran", "careened", "helped", "prostrated", "waited", "contemplated", "pontificated", "understood","climbed", "killed", "hurt", "walked", "stopped", "ate", "jumped", "decided", "considered", "planted", "swung", "pounced", "bit", "began","cried", "laughed", "doubled over"]
+,	"presverbs": ["writhe", "fade", "commiserate", "flow", "run", "careen", "help", "prostrate", "wait", "contemplate", "pontificate", "understand", "climb", "kill", "bleed", "hurt", "walk", "stop", "eat", "jump", "decide", "consider", "plant", "swing", "pounce", "bite", "begin", "cry", "laugh"]
+,	"nouns":  ["cypress", "carolina cherry", "pecan", "tonight", "clothespin", "anchor", "anaqua", "carriage", "tide", "disease", "frog", "moon", "toad", "basilisk", "spectacle", "eternity", "marriage", "rabbit", "farm", "cactus", "palace", "ember", "statue", "hat", "person", "human", "sponge", "night", "urchin", "porch", "cat", "dog", "mouse", "table", "plant", "building", "star", "suitor", "mollusk", "map", "sunset", "daffodil", "portrait", "river", "tree", "particle" ]
+, 	"plrnouns": ["cypresses", "carolina cherrys", "pecans", "nights", "clothespins", "anchors", "anaquas", "carriages", "tides", "diseases", "frogs", "moons", "toads", "basilisks", "spectacles", "eternities", "marriages", "rabbits", "farms", "cacti", "palaces", "embers", "statues", "hats", "persons", "humans", "sponges", "urchins", "porches", "cats", "dogs", "ice", "tables", "plants", "buildings", "stars", "suitors", "mollusks", "maps", "sunsets", "daffodils", "portraits", "rivers", "trees", "particles" ]
+,	"adjectives": ["tall", "fading", "synthetic", "broken", "bleeding", "sinister", "perishing", "vile","phosphorescent", "turqoise", "diseased", "aquatic", "maddening", "prostrate", "endless", "viscous", "friendly", "hateful", "special", "surely", "decrepit", "accidental", "mournful", "tricky", "spiteful", "beautiful", "frowning", "descending", "decayed", "sacharine", "loved", "rotten", "vaporous", "putrid", "glowing", "rambunctious", "hesitant", "eager", "shining", "dull", "glimmering", "peaceful", "profound"]
+
+}
